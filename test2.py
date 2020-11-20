ab5c54c23aad497fb666da38692b069ff016f097
Andrei <andreilukin88@gmail.com>
2020-11-20 14:16:01 +0300

    Jira's link must be here

--- test2.py
+++ test2.py
@@ -2,40 +2,56 @@ import requests
 import datetime
 import smtplib
 import pymysql
+import logging
 import pandas as pd
+from sys import argv
 from email.mime.multipart import MIMEMultipart     
 from email.mime.text import MIMEText     
 from email.mime.image import MIMEImage 
 
 
-a = datetime.datetime.now().date()
-b = a - datetime.timedelta(days=3)
+#Default time slice
+date_now = datetime.datetime.now().date()
+date_end = date_now - datetime.timedelta(days=3)
 
+def logining(txt):
+	"""Function for loginning"""
+	logging.basicConfig(
+						filename='test2.log', 
+						filemode='w', 
+						format='%(name)s - %(levelname)s - %(message)s'
+						)
+	logging.warning(txt)
+	
 def mail(err):
 	""" Function for sending mail"""
 	
 	#Data for autorization
-    addr_from = "herokusite@gmail.com"                 
-    addr_to   = "andreilukin88@gmail.com"                   
-    password  = "hb1641012"                                 
+	addr_from = "herokusite@gmail.com"                 
+	addr_to   = "andreilukin88@gmail.com"                   
+	password  = "hb1641012"                                 
 	
 	#Filling in the address and subject of the message
-    msg = MIMEMultipart()                               
-    msg['From']    = addr_from                          
-    msg['To']      = addr_to                            
-    msg['Subject'] = 'ALARM! Broken feed'                   
+	msg = MIMEMultipart()                               
+	msg['From']    = addr_from                          
+	msg['To']      = addr_to                            
+	msg['Subject'] = 'ALARM! Broken feed'                   
 	
 	#Message creating
-    body = 'Hi, i am your feed about forbes data, i have some problems. ' + str(err)
-    msg.attach(MIMEText(body, 'plain'))                 
+	body = 'Hi, i am your feed about forbes data, i have some problems. ' + str(err)
+	msg.attach(MIMEText(body, 'plain'))                 
 	
 	#Sending message
-    server = smtplib.SMTP('smtp.gmail.com', 587)           
-    server.set_debuglevel(True)                        
-    server.starttls()                                 
-    server.login(addr_from, password)
-    server.send_message(msg)
-    server.quit()
+	server = smtplib.SMTP('smtp.gmail.com', 587)           
+	server.set_debuglevel(True)                        
+	server.starttls()                                 
+	server.login(addr_from, password)
+	try:
+		server.send_message(msg)
+	except:
+		err = 'I lost your mail. ' + str(date_now)
+		logining(err)
+	server.quit()
     
 def db_add(df):
 	"""Function for creating data to DB"""
@@ -48,8 +64,9 @@ def db_add(df):
 									db='test'
 									)
 	except:
-		err = "I can't connect with DB server" + str(a)
+		err = "I can't connect with DB server" + str(date_now)
 		mail(err)
+		logining(err)
 		
 	cursor = connection.cursor()
 	
@@ -61,8 +78,9 @@ def db_add(df):
 			(info['date']))
 			connection.commit()
 	except:
-		err = "I can't delete data from DB. " + str(a)
+		err = "I can't delete data from DB. " + str(date_now)
 		mail(err)
+		logining(err)
 		
 	cols = "`,`".join([str(i) for i in df.columns.tolist()])
 	
@@ -73,8 +91,9 @@ def db_add(df):
 			cursor.execute(sql, tuple(row))
 			connection.commit()
 	except:
-		err = "I can't create data at DB. " + str(a)
+		err = "I can't create data at DB. " + str(date_now)
 		mail(err)
+		logining(err)
 		
 	result = cursor.fetchall()
 	connection.close()
@@ -98,17 +117,33 @@ def clear_data(dt, date):
 		     }
 	df1 = pd.DataFrame(frame)
 	db_add(df1)
-	
-#Connecting to API and getting raw data	
-while str(b) != str(a):
-	url = f'https://openexchangerates.org/api/historical/{str(b)}.json?app_id=c54e441aec6c4f42a2cb17d81a675cae'
-	try:
-		data = requests.get(url)
-	except:
-		err = "I can't get data on API. " + str(a)
-		mail(err)
-		break
-		
-	response_dict = data.json()
-	clear_data(response_dict, b)
-	b += datetime.timedelta(days=1)
+
+def start(now, end):	
+	"""Function for starting application"""
+	#Connecting to API and getting raw data
+	while str(now) != str(end):
+		url = f'https://openexchangerates.org/api/historical/{str(end)}.json?app_id=c54e441aec6c4f42a2cb17d81a675cae'
+		try:
+			data = requests.get(url)
+		except:
+			err = "I can't get data on API. " + str(date_now)
+			mail(err)
+			logining(err)
+			break
+		response_dict = data.json()
+		clear_data(response_dict, end)
+		end += datetime.timedelta(days=1)
+
+# Parameter processing
+try:
+	if argv[1:]:
+		date_par = ''
+		for i in argv[1:]:
+			date_par += i
+		start(date_now, datetime.datetime.strptime(date_par, "%Y-%m-%d").date())
+	else:
+		start(date_now, date_end)
+except:
+	err = "I can't start. " + str(date_now)
+	mail(err)
+	logining(err)
