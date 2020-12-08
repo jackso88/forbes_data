e8aa266a65d241b6fd5bde1e9d2148fc85826533
Andrei <andreilukin88@gmail.com>
2020-12-09 02:33:20 +0300

    Jira

--- plot.py
+++ plot.py
@@ -23,50 +23,32 @@ def read_from_DB(connection, sql):
     connection.close()
     return result
 
-def transform_data(data_t):
-    """ Data preparation function for visualization """
-    datas = {}
-    date = []
-    for element in data_t:
-        if element[0] in datas:
-            if isinstance(datas[element[0]], list):
-                datas[element[0]].append(element[1])
-            else:
-                datas[element[0]] = [datas[element[0]], element[1]]
-        else:
-            datas[element[0]] = element[1]
-        if element[2] not in date:
-            date.append(element[2])
-    datas['dates'] = date
-    return datas
 
-connect = ff.db_connect()
+def visual_data(data):
+    """ Data visualization """
+    line_chart = pygal.Line()
+    line_chart.title = 'Data visualization for last month'
+    line_chart.x_labels = map(str, [i[1] for i in data[::-1]])
+    line_chart.add(option, [i[0] for i in data[::-1]])
+    line_chart.render_to_file('chart.svg')
 
-txt = "\nEnter '0' for average value drawing or '1' for slicing data "
-option = int(input(txt))
+
+txt = "\nEnter 'average' for average value drawing or "
+txt += "country code for visualisation data "
+
+option = input(txt)
 
 table = config['db_add']['table']
-
-if option == 1:
-    sql = f"select * from {table} where date >= '{str(date_end)}' "
-    sql += f"and value between (select avg(value) from {table})*0.6 "
-    sql += f"and (select avg(value) from {table});"    
-    data = read_from_DB(connect, sql)
-    dict_d = transform_data(data)
-    line_chart = pygal.Line()
-    line_chart.title = 'Data slice visualization for last month'
-    line_chart.x_labels = map(str, dict_d['dates'])
-    for key, value in dict_d.items():
-        if key != 'dates':
-            line_chart.add(key, value)
-    line_chart.render_to_file('un_chart.svg')
     
-elif option == 0:
+if option == 'average':
     sql = f"select avg(value), date from {table} group by 2 order by 2 "
     sql += f"desc limit {config['common']['days_draw']}"
-    data = read_from_DB(connect, sql)
-    line_chart = pygal.Line()
-    line_chart.title = 'AVG value data visualization for last month'
-    line_chart.x_labels = map(str, [i[1] for i in data[::-1]])
-    line_chart.add('AVG value', [i[0] for i in data[::-1]])
-    line_chart.render_to_file('avg_chart.svg')
+    
+elif len(option) == 3:
+    sql = f"select value, date from {table} where country_id = '{option}'"
+    sql += f" order by date desc limit {config['common']['days_draw']}"
+
+connect = ff.db_connect()
+data = read_from_DB(connect, sql)
+visual_data(data)
+    
