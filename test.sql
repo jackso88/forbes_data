fe96e55e6dfaf808467cb47cce50efd8ae2d2e7d
Andrei <andreilukin88@gmail.com>
2020-12-08 16:58:31 +0300

    Jira

--- test.sql
+++ test.sql
@@ -22,21 +22,23 @@ BEGIN
 	WHILE str_date <= end_date DO
 		IF str_date NOT IN (SELECT date FROM calendar) THEN
 		INSERT INTO calendar VALUES(str_date, 
-        DAYOFMONTH(str_date), MONTH(str_date), YEAR(str_date), DAYNAME(str_date),
+        DAYOFMONTH(str_date),
+        MONTH(str_date), 
+        YEAR(str_date), 
+        DAYNAME(str_date),
         MONTHNAME(str_date), 
         IF(DAYOFMONTH(str_date)/10 < 1, 1, 
-        IF(DAYOFMONTH(str_date)/10 >= 1 AND DAYOFMONTH(str_date)/10 <2, 2, 3)),
+			IF(DAYOFMONTH(str_date)/10 >= 1 AND DAYOFMONTH(str_date)/10 <2, 2, 3)),
         IF(MONTH(str_date)/3 < 1, 1, 
-        IF(MONTH(str_date)/3 >= 1 AND MONTH(str_date)/3 < 2, 2, 
-        IF(MONTH(str_date)/3 >= 2 AND MONTH(str_date)/3 < 3, 3, 4))),
-        IF(DAYNAME(str_date) LIKE 'SUNDAY' 
-        OR DAYNAME(str_date) LIKE 'SATURDAY', 'YES', 'NO'));
+			IF(MONTH(str_date)/3 >= 1 AND MONTH(str_date)/3 < 2, 2, 
+				IF(MONTH(str_date)/3 >= 2 AND MONTH(str_date)/3 < 3, 3, 4))),
+        IF(DAYNAME(str_date) IN ('SUNDAY', 'SATURDAY'), 'YES', 'NO'));
         END IF;
-        SET str_date = DATE_ADD(str_date, interval 1 day);
+        SET str_date = DATE_ADD(str_date, INTERVAL 1 DAY);
 	END WHILE;
 END;$$
 DELIMITER ;
-
+ 
 -- Filling table with dates
 CALL calendar('2018-12-01', '2023-01-02');
 
