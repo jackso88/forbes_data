d4d58c908f53ed933af52c853fe32b020e1a549f
Andrei <andreilukin88@gmail.com>
2020-12-09 03:49:47 +0300

    Jira

--- plot.py
+++ plot.py
@@ -10,10 +10,15 @@ from sys import argv
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-# Default time slice
-date_now = datetime.now().date()
-date_end = date_now - timedelta(days=config['common']['days_draw'])
+txt = "Enter 'average' for average value drawing or three-digit "
+txt += "country code for visualization data.\nFor currency count visuali"
+txt += "zation enter 'count' "
 
+option = input(txt)
+
+txt_day = "\nHow many last days should I visualize? "
+
+day = input(txt_day)
 
 def read_from_DB(connection, sql):
     """ Function for reading data from DB """
@@ -28,31 +33,25 @@ def read_from_DB(connection, sql):
 def visual_data(data):
     """ Data visualization """
     line_chart = pygal.Line()
-    line_chart.title = 'Data visualization for last month'
+    line_chart.title = f'Data visualization for last {day} day(s)'
     line_chart.x_labels = map(str, [i[1] for i in data[::-1]])
     line_chart.add(option, [i[0] for i in data[::-1]])
     line_chart.render_to_file('chart.svg')
 
 
-txt = "Enter 'average' for average value drawing or three-digit "
-txt += "country code for visualization data.\nFor currency count visuali"
-txt += "zation enter 'count' "
-
-option = input(txt)
-
 table = config['db_add']['table']
 
 if option == 'average':
     sql = f"select avg(value), date from {table} group by 2 order by 2 "
-    sql += f"desc limit {config['common']['days_draw']}"
+    sql += f"desc limit {day}"
 
 elif option == 'count':
     sql = f"select count(country_id), date from {table} group by date "
-    sql += f"order by 2 desc limit {config['common']['days_draw']}"
+    sql += f"order by 2 desc limit {day}"
 
 elif len(option) == 3:
     sql = f"select value, date from {table} where country_id = '{option}'"
-    sql += f" order by date desc limit {config['common']['days_draw']}"
+    sql += f" order by date desc limit {day}"
 
 connect = ff.db_connect()
 data = read_from_DB(connect, sql)
