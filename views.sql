7bab28954897bc94f9429996e1bb9c2900ba5f89
Andrei <andreilukin88@gmail.com>
2020-12-10 18:39:21 +0300

    Jira

---
+++ views.sql
@@ -0,0 +1,21 @@
+CREATE VIEW wFluct AS
+SELECT country_id, 
+       (MAX(value)-MIN(value))/AVG(value) AS delta_coef,
+       calendar.week_num,
+       DENSE_RANK() OVER(PARTITION BY calendar.week_num 
+       ORDER BY ABS(1-(MAX(value)/AVG(value)))+ABS((MIN(value)/AVG(value))-1)) AS place
+       FROM forbes
+JOIN calendar ON calendar.date = forbes.date
+GROUP BY 1, 3;
+
+CREATE VIEW dFluct AS
+SELECT a.country_id, 
+       ABS((a.next_val-value)/value) AS delta, 
+       calendar.date, 
+       DENSE_RANK() OVER(PARTITION BY calendar.date ORDER BY ABS((a.next_val-value)/value)) AS place
+FROM
+	(SELECT forbes.country_id,
+            LEAD(value) OVER(ORDER BY value) AS next_val,
+            value, 
+            date FROM forbes)a
+JOIN calendar ON calendar.date = a.date;
